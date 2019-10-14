01ea0df2f86ae29f4a5bc41ce5543eedfac2c543
Pim <pim103@hotmail.fr>
2019-10-14 14:38:16 +0200

    Init Player Scripts

---
+++ TowerFallDlaMort/Assets/Scripts/Players/PlayerIntent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIntent : MonoBehaviour
+{
+    public bool wantToMoveBack;
+    public bool wantToMoveForward;
+    public bool wantToMoveRight;
+    public bool wantToMoveLeft;
+
+    public bool wantToShoot;
+
+    public bool wantToBlock;
+}
